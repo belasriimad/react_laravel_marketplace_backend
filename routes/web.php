9f88b69ac7cf741f46834a343df1b8515d1b36d7
Belasri Imad <imadbelasri@gmail.com>
2025-05-07 15:59:41 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -26,28 +26,28 @@ Route::prefix('admin')->group(function() {
     Route::middleware('admin')->group(function() {
         Route::get('dashboard', [AdminController::class, 'index'])->name('admin.index');
         Route::post('logout', [AdminController::class, 'logout'])->name('admin.logout');
+        Route::resource('categories', CategoryController::class, [
+            'names' => [
+                'index' => 'admin.categories.index',
+                'create' => 'admin.categories.create',
+                'store' => 'admin.categories.store',
+                'edit' => 'admin.categories.edit',
+                'update' => 'admin.categories.update',
+                'destroy' => 'admin.categories.destroy'
+            ]
+        ]);
+        //reviews routes
+        Route::get('reviews', [ReviewController::class, 'index'])->name('admin.reviews.index');
+        Route::get('edit/{review}/{status}/reviews', [ReviewController::class, 'toggleReviewStatus'])->name('admin.reviews.edit');
+        Route::delete('delete/{review}/reviews', [ReviewController::class, 'destroy'])->name('admin.reviews.destroy');
+        //pictures routes
+        Route::get('pictures', [PictureController::class, 'index'])->name('admin.pictures.index');
+        Route::get('edit/{picture}/{status}/pictures', [PictureController::class, 'togglePictureStatus'])->name('admin.pictures.edit');
+        //users routes
+        Route::get('users', [UserController::class, 'index'])->name('admin.users.index');
+        Route::delete('delete/{user}/users', [UserController::class, 'destroy'])->name('admin.users.destroy');
+        //orders routes
+        Route::get('orders', [OrderController::class, 'index'])->name('admin.orders.index');
+        Route::delete('delete/{order}/orders', [OrderController::class, 'destroy'])->name('admin.orders.destroy');
     });
-    Route::resource('categories', CategoryController::class, [
-        'names' => [
-            'index' => 'admin.categories.index',
-            'create' => 'admin.categories.create',
-            'store' => 'admin.categories.store',
-            'edit' => 'admin.categories.edit',
-            'update' => 'admin.categories.update',
-            'destroy' => 'admin.categories.destroy'
-        ]
-    ]);
-    //reviews routes
-    Route::get('reviews', [ReviewController::class, 'index'])->name('admin.reviews.index');
-    Route::get('edit/{review}/{status}/reviews', [ReviewController::class, 'toggleReviewStatus'])->name('admin.reviews.edit');
-    Route::delete('delete/{review}/reviews', [ReviewController::class, 'destroy'])->name('admin.reviews.destroy');
-    //pictures routes
-    Route::get('pictures', [PictureController::class, 'index'])->name('admin.pictures.index');
-    Route::get('edit/{picture}/{status}/pictures', [PictureController::class, 'togglePictureStatus'])->name('admin.pictures.edit');
-    //users routes
-    Route::get('users', [UserController::class, 'index'])->name('admin.users.index');
-    Route::delete('delete/{user}/users', [UserController::class, 'destroy'])->name('admin.users.destroy');
-    //orders routes
-    Route::get('orders', [OrderController::class, 'index'])->name('admin.orders.index');
-    Route::delete('delete/{order}/orders', [OrderController::class, 'destroy'])->name('admin.orders.destroy');
 });
